9c80e14dab111f840dbd5deacaeb9d2c7481ecb9
jeevabalanmadhu <jeevabalan.m@fadv.com>
2022-07-14 15:40:25 +0530

    ass

--- doozy/urls.py
+++ doozy/urls.py
@@ -7,8 +7,8 @@ from users import views as user_views
 from django.conf import settings
 from django.conf.urls.static import static
 
-from django.conf.urls import url
-from django.views.static import serve
+# from django.conf.urls import url
+# from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -35,8 +35,8 @@ urlpatterns = [
     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(
         template_name='users/password_change_done.html'), name='password_change_done'),
 
-    url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
+    # url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    # url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
 ]
 
 if settings.DEBUG:
